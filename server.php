934144624f98d9df75b410473810d042a242a1e9
OsmanChakhkiev <oempire70@gmail.com>
2023-03-22 18:25:17 +0300

    init: add php file with project

---
+++ server.php
@@ -0,0 +1,103 @@
+<?php
+
+
+   // Задача 1.
+
+   function multiply ($x){
+         return $x * 3;
+      }
+      echo multiply (5, 3);
+
+
+
+   // Задача 2.
+
+   function divide ($a, $b){
+         return $a / $b;
+      }
+      echo divide (10, 7);
+
+
+
+
+   // Задача 3.
+   
+   $name = 'Алиса';
+   $age = 25;
+   echo "Меня зовут $name и мне $age лет";
+
+
+
+
+   // Задача 4.
+   
+   function multiply ($x){
+         return $x * 2;
+      }
+      echo multiply (12, 2);
+   
+
+
+
+   // Задача 5.
+
+   function multiply ($x, $y){
+         return $x * $y;
+      }
+      echo multiply (6, 4);
+
+
+
+
+   // Задача 6.  
+
+   function addition ($a, $b){
+         return $a + $b;
+      }   
+      echo addition (7, 3);
+
+
+
+
+   // Задача 7.
+   
+   $frameWork = ' .Net';
+   echo 'Мне нравится программирование на' . $frameWork;
+
+
+
+
+   
+   // Задача 8.
+
+   $a = 23;
+   $a++;
+   $result = $a;
+   echo $result;  
+   
+
+
+
+
+   // Задача 9.
+
+   $userName = "Jacob";
+   $userSecondName = "Seed";
+
+   $userInfo = $userName . $userSecondName;
+
+   echo $userInfo;
+
+
+   // Задача 10.
+
+   $strNum = "21";
+   $num = 3;
+
+   $result = $strNum + $num;
+
+   echo $result;
+
+   
+   
+   
